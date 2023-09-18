757459f02a53ff70829c2a1dd54a6c8bb1c744a1
alejox <alejoxdrums@gmail.com>
2023-09-18 11:47:50 -0500

    add back btn

---
+++ src/components/global/ui/BackDummy.vue
@@ -0,0 +1,38 @@
+<template>
+  <article class="back">
+    <div>
+      <img src="@/assets/icons/form/back.svg" alt="" />
+    </div>
+    <p class="b-regular back__text">{{ $t('globals.back') }}</p>
+  </article>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  num: {
+    type: [Number, String],
+    default: 1
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.back {
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  gap: 8px;
+  height: 36px;
+  cursor: pointer;
+  background: #f6f8fa;
+  border: 1px solid #dde4ed;
+  border-radius: 8px;
+  width: fit-content;
+  &__text {
+    display: none;
+    @media (min-width: 600px) {
+      display: unset;
+    }
+  }
+}
+</style>
---
+++ src/components/global/ui/GoBack.vue
@@ -0,0 +1,40 @@
+<template>
+  <article class="back" @click="$router.back()">
+    <div>
+      <img src="@/assets/icons/form/back.svg" alt="" />
+    </div>
+    <p class="b-regular">{{ $t('globals.back') }}</p>
+  </article>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    back: {
+      Type: String,
+      default: '/'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.back {
+  display: flex;
+  align-items: center;
+  padding: 8px;
+  gap: 8px;
+  height: 36px;
+  cursor: pointer;
+  background: #f6f8fa;
+  border: 1px solid #dde4ed;
+  border-radius: 8px;
+  width: fit-content;
+  &__text {
+    display: none;
+    @media (min-width: 600px) {
+      display: unset;
+    }
+  }
+}
+</style>
