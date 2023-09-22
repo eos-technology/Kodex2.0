cc7f066ff75198b29a338c32cad0cc7a33c3d0f1
ArielDeAvilaEOS <arieldeavila_1996@hotmail.com>
2023-09-22 14:54:42 -0500

    first update

---
+++ src/modules/cart/views/cart/Index.vue
@@ -0,0 +1,15 @@
+<template> 
+    <v-card>
+        <v-card-item>
+            hola mundo
+        </v-card-item>
+    </v-card>
+</template> 
+
+<script lang='ts' setup> 
+
+</script> 
+
+<style lang='scss' scoped> 
+
+</style>
