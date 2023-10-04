1f4232fa38945bc327175999930fdeb69f6c08dd
alejox <alejoxdrums@gmail.com>
2023-10-04 16:11:56 -0500

    update theme

--- src/plugins/VuetifyThemes.ts
+++ src/plugins/VuetifyThemes.ts
@@ -20,13 +20,13 @@ export const light: ThemeDefinition = {
 export const dark: ThemeDefinition = {
   dark: true,
   colors: {
-    background: '#3700B3',
-    surface: '#131313',
-    primary: '#EBBA4B',
-    'primary-darken-1': '#3700B3',
+    background: '#F9FAFB',
+    surface: '#fff',
+    primary: '#0A1E8C',
+    'primary-darken-1': '#001E62',
     input: '#F6F7F9',
     secondary: '#397EA6',
-    'secondary-darken-1': '#018786',
+    'secondary-darken-1': '#3587FF',
     error: '#B42318',
     info: '#2196F3',
     success: '#47CD89',
